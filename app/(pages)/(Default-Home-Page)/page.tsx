8cdf9bac80ae6bbb91282f89dde22a790a8ed76c
hamza <=>
2026-09-03 21:14:34 +0500

    a

--- app/(pages)/(Default-Home-Page)/page.tsx
+++ app/(pages)/(Default-Home-Page)/page.tsx
@@ -1,33 +1,15 @@
 import Image from 'next/image'
 import sectionImage from "../../public/images/scandinavian-interior-mockup-wall-decal-background 1.png"
-import diningPhoto from "../../public/images/Mask Group.png"
-import LivingPhoto from "../../public/images/image 100.png"
-import BedroomPhoto from "../../public/images/image 101.png"
+
 // import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
-import pro1 from "../../public/images/Images.png"
-import pro2 from "../../public/images/image 2.png"
-import pro3 from "../../public/images/image 3.png"
-import pro4 from "../../public/images/Images(1).png"
-import pro5 from "../../public/images/Images(2).png"
-import pro6 from "../../public/images/Images(3).png"
-import pro7 from "../../public/images/Images(4).png"
-import pro8 from "../../public/images/image 8.png"
+import {IMAGES_DATA,PRODUCT_IMAGE_DATA,rect36,rect37,rect38,rect39,rect40,rect41,rect43,rect44,rect45} from "../../utils/index"
 
-import Label1 from "../../public/images/Label.png"
 import ProductCard from '@/app/Components/ProductCard'
 
-import rect36 from "../../public/images/Furniture-image/Rectangle 36.png"
-import rect38 from "../../public/images/Furniture-image/Rectangle 38.png"
-import rect37 from "../../public/images/Furniture-image/Rectangle 37.png"
-import rect39 from "../../public/images/Furniture-image/Rectangle 39.png"
-import rect40 from "../../public/images/Furniture-image/Rectangle 40.png"
-import rect44 from "../../public/images/Furniture-image/Rectangle 44.png"
-import rect41 from "../../public/images/Furniture-image/Rectangle 41.png"
-import rect43 from "../../public/images/Furniture-image/Rectangle 43.png"
-import rect45 from "../../public/images/Furniture-image/Rectangle 45.png"
+
 // import carousalImg1 from "../../public/images/carousle/Image.png"
 // import carousalImg2 from "../../public/images/carousle/Rectangle 25.png"
 
@@ -48,88 +30,9 @@ import rect45 from "../../public/images/Furniture-image/Rectangle 45.png"
 
 
 
-export { diningPhoto, LivingPhoto, BedroomPhoto, sectionImage }
+// export {  sectionImage }
 
-const IMAGES_DATA = [
-  { id: 1, image: diningPhoto, text: "Dining" },
-  { id: 2, image: LivingPhoto, text: "Living" },
-  { id: 3, image: BedroomPhoto, text: "Bedroom" }
-];
 
-const PRODUCT_IMAGE_DATA = [
-  {
-    id: 1,
-    Label: Label1,
-    image: pro1,
-    heading: "Syltherine",
-    paragraph: "Stylish cafe chair",
-    price: "Rp 2.500.000",
-    cutPrice: "Rp 3.500.000",
-  },
-  {
-    id: 2,
-
-    image: pro2,
-    heading: "Leviosa",
-    paragraph: "Stylish cafe chair",
-    price: "Rp 2.500.000",
-  },
-  {
-    id: 3,
-    Label: Label1,
-
-    image: pro3,
-    heading: "Lolito",
-    paragraph: "Luxury big sofa",
-    price: "Rp 7.000.000",
-    cutPrice: "Rp 14.000.000"
-
-  },
-  {
-    id: 4,
-    Label: Label1,
-
-    image: pro4,
-    heading: "Respira",
-    paragraph: "Outdoor bar table and stool",
-    price: "Rp 500.000"
-  },
-  {
-    id: 5,
-    image: pro5,
-    heading: "Grifo",
-    paragraph: "Night lamp",
-    price: "Rp 1.500.000"
-  },
-  {
-    id: 6,
-    Label: Label1,
-
-    image: pro6,
-    heading: "Muggo",
-    paragraph: "Small mug",
-    price: "Rp 150.000"
-  },
-  {
-    id: 7,
-    Label: Label1,
-
-    image: pro7,
-    heading: "Pingky",
-    paragraph: "Cute bed set",
-    price: "Rp 7.000.000",
-    cutPrice: "Rp 14.000.000"
-  },
-  {
-    id: 8,
-    Label: Label1,
-
-    image: pro8,
-    heading: "Potty",
-    paragraph: "Minimalist flower pot",
-    price: "Rp 500.000"
-  }
-]
 
 export default function Page() {
   return (
